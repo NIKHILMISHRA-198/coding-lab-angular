10fbeabdc30ccd7d5a6f94d40e194ae4a874a6f3
Nikhil Mishra <nikhil@Nikhils-MacBook-Air.local>
2023-04-27 14:18:31 +0530

    first commit

---
+++ src/app/user.ts
@@ -0,0 +1,14 @@
+export class User {
+    name: string;
+    email: string;
+    mobileNumber: number;
+    location: string;
+    constructor(name: string, email: string, mobileNumber: number, location: string) {
+        this.name = name;
+        this.email = email;
+        this.mobileNumber =mobileNumber;
+        this.location = location;
+    }
+}
+
+
